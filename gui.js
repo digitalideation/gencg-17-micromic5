cb04bcb2e694d309b734796ea5827995a617fdbe
micromic5 <mike.amacker@gmx.ch>
2017-12-15 11:48:50 +0100

    changed parameters

--- gui.js
+++ gui.js
@@ -1,18 +1,18 @@
 let options = {
-	randomSeed: 2018,
-	numberOfAgents: 1,
-	moveSpeed: 2,
+	randomSeed: 2015,
+	numberOfAgents: 3,
+	moveSpeed: 3,
 	// maxSpeed: 5,
 	agentFatness: 3,
 	// turnRadius: Math.PI/32,
 	timeToLive: 100,
 	// tileWidth: 200,
 	// tileHeight: 200,
-	tileWidth: 150,
+	tileWidth: 120,
 	tileHeight: 150,
 	agentColor: [66, 230, 220], //RGB
 	backgroundColor: [0, 0, 0], //RGB
-	backgroundAlpha: 30, //0 - 100
+	backgroundAlpha: 20, //0 - 100
 	randomPlacement: false,
 	useRadius: false,
 	sendToNeighbor: true,
