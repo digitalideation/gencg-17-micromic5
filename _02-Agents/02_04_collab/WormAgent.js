11227f0a69a880a1a0e7d503c72f40cbee03acd6
micromic5 <mike.amacker@gmx.ch>
2017-12-22 09:44:04 +0100

    clean up

--- _02-Agents/02_04_collab/WormAgent.js
+++ _02-Agents/02_04_collab/WormAgent.js
@@ -169,11 +169,7 @@ class WormAgent extends MasterAgent
 			let aSquared = Math.pow(Math.abs(newX - this.location.x), 2);
 			let bSquared = Math.pow(Math.abs(newY - this.location.y), 2);
 			let cSquared = Math.pow(this.radius, 2);
-			if (aSquared + bSquared < cSquared)
-			{
-				// all normal
-			}
-			else
+			if(aSquared + bSquared >= cSquared)
 			{
 				// set the pos back a step
 				newX = this.points[this.points.length - 1].x;
@@ -193,67 +189,8 @@ class WormAgent extends MasterAgent
 
 				// kill
 				this.agentAlive = false;
-
-				/*
-				if (this.angle > 0
-					&& (this.angle < Math.PI
-						|| this.angle > Math.PI * 3/2))
-				{
-					this.decrementAngle();
-				}
-				else
-					*/
-
-				// if (this.angle >= 0
-				// 	&& this.angle < (Math.PI / 2))
-				// {
-				// 	// bottom right direction
-				// 	// this.angle += angleStep;
-				// 	this.incrementAngle();
-				// }
-				// else if (this.angle >= (Math.PI / 2)
-				// 	&& this.angle < Math.PI)
-				// {
-				// 	// bottom left direction
-				// 	// this.angle += angleStep;
-				// 	this.incrementAngle();
-				// }
-				// else if (this.angle >= Math.PI
-				// 	&& this.angle < (2 * Math.PI * 2 / 3))
-				// {
-				// 	// top left direction
-				// 	// this.angle -= angleStep;
-				// 	this.incrementAngle();
-				// }
-				// else if (this.angle >= (2 * Math.PI * 2 / 3)
-				// 	&& this.angle < Math.PI * 2)
-				// {
-				// 	// top right direction
-				// 	// this.angle += angleStep;
-				// 	this.incrementAngle();
-				// }
-				// else
-				// {
-				// 	this.angle += angleStep;
-				// }
 			}
-			// if (newX < (this.location.x - this.radius))
-			// {
-			// 	newX = this.location.x - this.radius;
-			// }
-			// else if (newX > (this.location.x + this.radius))
-			// {
-			// 	newX = this.location.x + this.radius;
-			// }
 
-			// if (newY < (this.location.y - this.radius))
-			// {
-			// 	newY = this.location.y - this.radius;
-			// }
-			// else if (newY > (this.location.y + this.radius))
-			// {
-			// 	newY = this.location.y + this.radius;
-			// }
 			if (this.angle > Math.PI * 2 || this.angle < 0)
 			{
 				this.agentAlive = false;
@@ -307,20 +244,6 @@ class WormAgent extends MasterAgent
 			}
 			else if (options.sendToNeighbor)
 			{
-				// if wall is hit
-				// if (this.sendToNeighbor &&
-				// 	(this.hitTop
-				// 	|| this.hitBottom
-				// 	|| this.hitLeft
-				// 	|| this.hitRight)
-				// 	&& !(this.hitTopWindowBorder
-				// 		|| this.hitRightWindowBorder
-				// 		|| this.hitBottomWindowBorder
-				// 		|| this.hitLeftWindowBorder))
-				// {
-				// 	this.sendToNeighborCell(newX, newY);
-				// }
-				// if wall is hit
 				if ((this.hitTop
 					|| this.hitBottom
 					|| this.hitLeft
@@ -333,23 +256,6 @@ class WormAgent extends MasterAgent
 					this.sendToNeighborCell(newX, newY);
 				}
 			}
-
-			// if (this.angle >= 0 && this.angle < Math.PI / 2)
-			// {
-			// 	// bottom right direction
-			// }
-			// else if (this.angle >= Math.PI / 2 && this.angle < Math.PI)
-			// {
-			// 	// bottom left direction
-			// }
-			// else if (this.angle >= Math.PI && this.angle < 2 * Math.PI / 3)
-			// {
-			// 	// top left direction
-			// }
-			// else
-			// {
-			// 	// top right direction
-			// }
 
 			this.customBehaviour();
 		}
@@ -398,31 +304,24 @@ class WormAgent extends MasterAgent
 			this.hitTopWindowBorder = true;
 		}
 
-		if (this.useRadius)
+		// left local border
+		if (xCoord < (this.location.x - this.tileWidth / 2))
 		{
-			// TODO
+			this.hitLeft = true;
 		}
-		else
+		else if (xCoord > (this.location.x + this.tileWidth / 2))
 		{
-			// left local border
-			if (xCoord < (this.location.x - this.tileWidth / 2))
-			{
-				this.hitLeft = true;
-			}
-			else if (xCoord > (this.location.x + this.tileWidth / 2))
-			{
-				this.hitRight = true;
-			}
+			this.hitRight = true;
+		}
 
-			// top local Border
-			if (yCoord < (this.location.y - this.tileHeight / 2))
-			{
-				this.hitTop = true;
-			}
-			else if (yCoord > (this.location.y + this.tileHeight / 2))
-			{
-				this.hitBottom = true;
-			}
+		// top local Border
+		if (yCoord < (this.location.y - this.tileHeight / 2))
+		{
+			this.hitTop = true;
+		}
+		else if (yCoord > (this.location.y + this.tileHeight / 2))
+		{
+			this.hitBottom = true;
 		}
 	}
 
@@ -473,16 +372,6 @@ class WormAgent extends MasterAgent
 		}
 
 		if (options.bounceOffLocalBorder
-			&& this.useRadius
-			&& (this.hitTop
-			|| this.hitBottom
-			|| this.hitLeft
-			|| this.hitRight))
-		{
-			// TODO
-			// using radius
-		}
-		else if (options.bounceOffLocalBorder
 			&& !this.useRadius
 			&& (this.hitTop
 			|| this.hitBottom
